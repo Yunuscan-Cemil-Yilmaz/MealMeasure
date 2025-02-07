0221aab5239fa0fd74b2ec6072ccbd0bcde84e69
Yunuscan-Cemil-Yilmaz <yilmazyunuscancemil00@gmail.com>
2025-02-07 14:21:39 +0300

    designed app module

--- web-front/src/app/app.module.ts
+++ web-front/src/app/app.module.ts
@@ -3,12 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthicencationModule } from './authicencation/authicencation.module';
+import { DietsModule } from './diets/diets.module';
+import { MealsModule } from './meals/meals.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AuthicencationModule,
+    DietsModule,
+    MealsModule,
+    SharedModule,
+
     BrowserModule,
     AppRoutingModule,
     HttpClientModule    
