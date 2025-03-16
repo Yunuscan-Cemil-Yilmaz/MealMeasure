c06c50cdf9b12b899b084b3fbd69b7c2ca785b5f
Yunuscan-Cemil-Yilmaz <yilmazyunuscancemil00@gmail.com>
2025-03-17 02:56:23 +0300

    add User model with fillable attributes and timestamp methods

---
+++ back/app/Models/Auth/User.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Models\Auth;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $table = 'users';
+    protected $primaryKey = 'user_id';
+    public $incrementing = true;
+
+    protected $fillable = [
+        'user_email',
+        'is_admin',
+        'user_name',
+        'user_surname',
+        'user_nickname',
+        'user_password',
+        'user_bmi',
+        'user_tdee',
+        'user_fav_diet_ids',
+        'user_diet_id',
+    ];
+
+    protected $hidden = [
+        'user_password'
+    ];
+
+    protected $casts = [
+        'is_admin' => 'boolean',
+        'user_bmi' => 'double',
+        'user_tdee' => 'double',
+    ];
+
+    public function getUserPassword() { return $this->user_password; }
+
+    // functions : (how to use ?) : 
+    
+    // Add data:
+    // $user = new User;
+    // $user->user_name = 'Ahmet';
+    // $user->user_surname = 'Yılmaz';
+    // $user->user_email = 'ahmet@example.com';
+    // $user->user_password = bcrypt('parola123'); // dont use just bycript !!
+    // $user->save();
+
+    // Get data:
+    // $users = User::all(); // Get all users
+    // $user = User::find(1); // get user with id 1
+    // $users = User::where('user_name', 'Ahmet')->get(); // get user with name : 'Ahmet'
+
+    // Data update: 
+    // $user = User::find(1);
+    // $user->email = 'new_email@example.com';
+    // $user->save();
+
+    // get timestamps : 
+    public function getCreatedAtColumn() { return $this->created_at; }
+    public function getUpdatedAtColumn() { return $this->updated_at; }
+    // how to use ? :
+    // $user = User::find(1) // get user with id 1
+    // if($user){
+    //     $createdAt = $user->getCreatedAtColumn();
+    //     $updatedAt = $user->getUpdatedAtColumn();
+    //     Log::info('Create user date: ' . $createdAt);
+    //     Log::info('Update user date: ' . $updatedAt);
+    // }
+}
