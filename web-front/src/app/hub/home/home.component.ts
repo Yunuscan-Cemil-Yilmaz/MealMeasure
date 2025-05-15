28a8e63d6d6cc329b49617e00fb77d15b1d870c6
Yunuscan-Cemil-Yilmaz <yilmazyunuscancemil00@gmail.com>
2025-05-15 21:46:22 +0300

    add user meal day seeder; enhance calendar navigation and date selection

--- web-front/src/app/hub/home/home.component.ts
+++ web-front/src/app/hub/home/home.component.ts
@@ -10,9 +10,23 @@ export class HomeComponent implements OnInit {
 
   days: number[] = [];
   selectedDay: number | null = null;
-
+  selectedMonth: string = "";
+  selectedYear: number = 0;
+  calculatedMonth: number = 0;
+  numberOfSelectedMonth: number = 0;
+  calculatedYear: number = 0;
+  months = [
+    'Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran',
+    'Temmuz', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık'
+  ];
+  selectedDateString: string = ""
   selectDay(day: number) {
-  this.selectedDay = day;
+    this.selectedDay = day;
+    const choosedDay = day >= 10 ? String(day) : String(`0${day}`); 
+    const choosedYear = this.selectedYear;
+    const choosedMonth = this.numberOfSelectedMonth >= 10 ? this.numberOfSelectedMonth : `0${this.numberOfSelectedMonth}`
+    this.selectedDateString = `${choosedYear}-${choosedMonth}-${choosedDay}`
+    console.log(this.selectedDateString);
   }
 
   goToSettings() {
@@ -23,8 +37,56 @@ export class HomeComponent implements OnInit {
   constructor(private router:Router) { }
 
   ngOnInit(): void {
-    const totalDaysInMonth = 31; 
-    this.days = Array.from({ length: totalDaysInMonth }, (_, i) => i + 1);
+    const today = new Date();
+    this.calculatedMonth = today.getMonth();
+    this.calculatedYear = today.getFullYear();
+    
+    this.selectedYear = this.calculatedYear;
+    this.selectedMonth = this.months[this.calculatedMonth];
+    this.numberOfSelectedMonth = this.calculatedMonth + 1;
+    
+    const lastDay = new Date(this.selectedYear, this.calculatedMonth + 1, 0);
+    const dayAmount = lastDay.getDate()
+    this.days = Array.from({ length: dayAmount }, (_, i) => i + 1);
+  }
+
+
+  nextMonth(){
+    if(this.calculatedMonth > 10){ // go next year
+      this.calculatedYear++;
+      this.selectedYear = this.calculatedYear;
+      this.calculatedMonth = 0;
+      this.selectedMonth = this.months[this.calculatedMonth];
+      this.numberOfSelectedMonth = this.calculatedMonth + 1;
+      this.daysLoop();
+    }else { // stay this year
+      this.calculatedMonth++;
+      this.selectedMonth = this.months[this.calculatedMonth];
+      this.numberOfSelectedMonth = this.calculatedMonth + 1;
+      this.daysLoop();
+    }
+  }
+
+  prevMonth(){
+    if(this.calculatedMonth < 1) { // go prev year
+      this.calculatedYear--;
+      this.selectedYear = this.calculatedYear;
+      this.calculatedMonth = 11;
+      this.selectedMonth = this.months[this.calculatedMonth];
+      this.numberOfSelectedMonth = this.calculatedMonth + 1;
+      this.daysLoop();
+    } else { // stay this year
+      this.calculatedMonth--
+      this.selectedMonth = this.months[this.calculatedMonth];
+      this.numberOfSelectedMonth = this.calculatedMonth + 1;
+      this.daysLoop();
+    }
+  }
+
+  daysLoop(){
+    const lastDay = new Date(this.selectedYear, this.calculatedMonth + 1, 0);
+    const dayAmount = lastDay.getDate()
+    this.days = Array.from({ length: dayAmount }, (_, i) => i + 1);
   }
 
 }
